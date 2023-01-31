e9021105fcbcf8f4c2ef78babaf96f94fa48013b
Phuc Nguyen <66092633+Saotalangim161099@users.noreply.github.com>
2023-01-31 14:24:09 -0500

    Create Node

---
+++ src/DLLNode.java
@@ -0,0 +1,35 @@
+public class DLLNode {
+    int data;
+    DLLNode prev;
+    DLLNode next;
+
+    public DLLNode(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public DLLNode getPrev() {
+        return prev;
+    }
+
+    public void setPrev(DLLNode prev) {
+        this.prev = prev;
+    }
+
+    public DLLNode getNext() {
+        return next;
+    }
+
+    public void setNext(DLLNode next) {
+        this.next = next;
+    }
+}
